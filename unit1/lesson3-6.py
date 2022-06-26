d388cb31973231ddeefc5217412b45cf59e10e9e
Arseny <arrrseny11@gmail.com>
2022-06-26 11:16:31 +0300

    add lesson3-6

---
+++ unit1/lesson3-6.py
@@ -0,0 +1,18 @@
+'''https://stepik.org/lesson/701973/step/9?unit=702074'''
+
+
+class TravelBlog:
+    total_blogs = 0
+
+
+tb1 = TravelBlog()
+tb1.name = 'Франция'
+tb1.days = 6
+
+TravelBlog.total_blogs += 1
+
+tb2 = TravelBlog()
+tb2.name = 'Италия'
+tb2.days = 5
+
+TravelBlog.total_blogs += 1
