6f0de7d4b72d9e571e51f5a21792bf36263cf4cb
Arseny <arrrseny11@gmail.com>
2022-06-26 11:13:09 +0300

    add lesson3-4

---
+++ unit1/lesson3-4.py
@@ -0,0 +1,11 @@
+'''https://stepik.org/lesson/701973/step/7?unit=702074'''
+
+
+class Notes:
+    uid = 1005435
+    title = 'Шутка'
+    author = 'И.С. Бах'
+    pages = 2
+
+
+print(getattr(Notes, 'author'))
