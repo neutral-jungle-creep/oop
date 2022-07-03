3a1544dd3e4ef64275ebfccfd41c19aa867c7a6c
neutral-jungle-creep <arrrseny11@gmail.com>
2022-07-03 23:35:57 +0300

    add lesson7-1

---
+++ unit1/1-7/lesson7-1.py
@@ -0,0 +1,29 @@
+'''https://stepik.org/lesson/701976/step/10?unit=702077'''
+from loguru import logger
+
+
+class Factory:
+    @staticmethod
+    def build_sequence():
+        logger.info(f'Вызов build_sequence')
+        return []
+
+    @staticmethod
+    def build_number(string):
+        logger.info(f'Вызов build_number')
+        return int(string)
+
+
+class Loader:
+    @staticmethod
+    def parse_format(string, factory):
+        seq = factory.build_sequence()
+        for sub in string.split(","):
+            item = factory.build_number(sub)
+            seq.append(item)
+
+        return seq
+
+
+res = Loader.parse_format("1, 2, 3, -5, 10", Factory)
+logger.info(f'res == {res}, type == {type(res)}')
